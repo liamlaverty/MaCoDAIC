abc8b27dbd9f0d8089106b6df1e4a6c504747b77
Liam Laverty <liamlaverty@gmail.com>
2023-03-25 13:26:19 +0000

    starts to add a unit test project

---
+++ Macodaic.App/Macodaic.App.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Macodaic.App.Core.Models;
+
+
+namespace Macodaic.App.Tests
+{
+    [TestClass]
+    public class AgentTests
+    {
+        // Test if the Agent class is abstract
+        // 
+        [TestMethod]
+        public void Agent_IsAbstract()
+        {
+            // Arrange
+            var agent = new Agent();
+            // Act
+            var isAbstract = agent.GetType().IsAbstract;
+            // Assert
+            Assert.IsTrue(isAbstract);
+        }
+
+
+        
+    }
+}
